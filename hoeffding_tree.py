be01683ec6a79f15e8052a0898cc508fdc1d2538
Haoyin Xu <hax042@ucsd.edu>
2020-10-02 21:39:02 -0400

    Try sklearn DecisionTreeClassifier

---
+++ hoeffding_tree.py
@@ -0,0 +1,16 @@
+'''
+Main Author: Haoyin Xu
+Corresponding Email: haoyinxu@gmail.com
+'''
+# import the necessary packages
+import numpy as np
+import pandas as pd
+
+from sklearn import tree
+
+# define the Hoeffding Decision Tree
+def hdt(X, y):
+    model = tree.DecisionTreeClassifier()
+    model.fit(X, y)
+
+    tree.plot_tree(model,filled=True)

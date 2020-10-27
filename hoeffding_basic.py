2b7cee6f81762db5c3bde225f764e2e241dc8d37
Haoyin Xu <haoyinxu@gmail.com>
2020-10-27 00:33:41 -0400

    Add Hoeffding bound equation

--- hoeffding_basic.py
+++ hoeffding_basic.py
@@ -4,12 +4,13 @@ Corresponding Email: haoyinxu@gmail.com
 """
 
 # import the necessary packages
+import math
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.tree import DecisionTreeClassifier
 
 
-def hoeffding_tree(X, y):
+def hoeffding_batch(X, y, confidence):
     """
     Create a basic hoeffding tree trained by data X with labels y.
 
@@ -19,10 +20,21 @@ def hoeffding_tree(X, y):
         Input data matrix.
     y : ndarray
         Output (i.e. response data matrix).
+    confidence: int
+                Probability of Hoeffding bound's correctness
     """
+    n_samples, n_features = X.shape
+    range = 1
+
+    # calculate the Hoeffding bound
+    ht_bound = math.sqrt((range**2) * math.log(1/(1-confidence)) / (2*n_samples))
 
     # create an empty decision tree
-    ht = DecisionTreeClassifier()
+    ht = DecisionTreeClassifier(max_features=2, min_impurity_decrease=ht_bound)
+    print(ht_bound)
+
+    # fit the Hoeffding batch tree
+    ht.fit(X, y)
 
     # return the fitted decision tree
     return ht

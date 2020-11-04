420626352fc1b2d29f848203501c3f47d39ada82
Haoyin Xu <haoyinxu@gmail.com>
2020-11-04 01:56:17 -0500

    Integrate both algorithms into package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+REQUIREMENTS = ["proglearn"]
+
+with open("README.md", mode="r", encoding="utf8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="spdt",
+    version="0.0.1",
+    author="Haoyin Xu",
+    asuthor_email="haoyinxu@gmail.com",
+    description="Streaming Parallel Decision Tree",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PSSF/SPDT/",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=REQUIREMENTS,
+)

6f1d4273e8d835bd6e36287f982dd6f90addfa6a
Haoyin Xu <haoyinxu@gmail.com>
2021-01-20 22:17:23 -0500

    Add NaiveStreamForest classifier

--- spdt/forest.py
+++ spdt/forest.py
@@ -4,6 +4,7 @@ Corresponding Email: haoyinxu@gmail.com
 """
 # import the necessary packages
 from scipy import stats
+from numpy.random import permutation
 
 # NOTE: the sklearn dependence is based on
 # personal fork and not corresponding to
@@ -14,6 +15,80 @@ from sklearn.utils.validation import (
     check_array,
 )
 
+
+class NaiveStreamForest:
+    """
+    A class used to represent a naive ensemble of
+    random stream decision trees.
+
+    Attributes
+    ----------
+    n_estimators : int
+        An integer that represents the amount of stream decision trees.
+
+    forest_ : list of sklearn.tree.DecisionTreeClassifier
+        An internal list that contains random
+        sklearn.tree.DecisionTreeClassifier.
+    """
+
+    def __init__(self, n_estimators=100):
+        self.forest_ = []
+
+        for i in range(n_estimators):
+            tree = DecisionTreeClassifier()
+            self.forest_.append(tree)
+
+    def fit(self, X, y):
+        """
+        Fits the forest to data X with labels y.
+
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        y : ndarray
+            Output (i.e. response data matrix).
+
+        Returns
+        -------
+        self : TreeClassificationTransformer
+            The object itself.
+        """
+        X, y = check_X_y(X, y)
+
+        # Update stream decision trees with random inputs
+        for tree in self.forest_:
+            p = permutation(X.shape[0])
+            X_r = X[p]
+            y_r = y[p]
+            tree.partial_fit(X_r, y_r)
+
+        return self
+
+    def predict(self, X):
+        """
+        Performs inference using the forest.
+
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+
+        Returns
+        -------
+        major_result : ndarray
+            The majority predictions.
+        """
+        X = check_array(X)
+
+        results = []
+        for tree in self.forest_:
+            result = tree.predict(X)
+            results.append(result)
+
+        major_result = stats.mode(results)[0][0]
+
+        return major_result
 
 class CascadeStreamForest:
     """
@@ -50,11 +125,11 @@ class CascadeStreamForest:
 
         # Update existing stream decision trees
         for tree in self.forest_:
-            tree.fit(X, y, update_tree=True)
+            tree.partial_fit(X, y)
 
         # Add a new decision tree based on new data
         sdt = DecisionTreeClassifier()
-        sdt.fit(X, y)
+        sdt.partial_fit(X, y)
         self.forest_.append(sdt)
 
         return self

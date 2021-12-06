987425fa4b526fe35a8375077926849b331f0579
Haoyin Xu <haoyinxu@gmail.com>
2021-12-06 11:24:51 -0500

    ENH & FIX switch to json files

--- benchmarks/code/cc18.py
+++ benchmarks/code/cc18.py
@@ -4,18 +4,12 @@ Author: Haoyin Xu
 import time
 import argparse
 import numpy as np
+import json
 import openml
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sdtf import StreamDecisionForest
-
-
-def write_result(filename, acc_ls):
-    """Writes results to specified text file"""
-    output = open(filename, "a")
-    for acc in acc_ls:
-        output.write(str(acc) + "\n")
 
 
 def prediction(classifier, X_test, y_test):
@@ -45,7 +39,7 @@ def experiment_rf(X_train, X_test, y_train, y_test):
 
         # Test the model
         start_time = time.perf_counter()
-        rf_l.append(prediction(rf, y_train, y_test))
+        rf_l.append(prediction(rf, X_test, y_test))
         end_time = time.perf_counter()
         test_time_l.append(end_time - start_time)
 
@@ -73,7 +67,7 @@ def experiment_sdf(X_train, X_test, y_train, y_test):
 
         # Test the model
         start_time = time.perf_counter()
-        sdf_l.append(prediction(sdf, y_train, y_test))
+        sdf_l.append(prediction(sdf, X_test, y_test))
         end_time = time.perf_counter()
         test_time_l.append(end_time - start_time)
 
@@ -95,6 +89,14 @@ args = parser.parse_args()
 
 BATCH_SIZE = 100
 
+rf_acc_dict = {}
+rf_train_t_dict = {}
+rf_test_t_dict = {}
+
+sdf_acc_dict = {}
+sdf_train_t_dict = {}
+sdf_test_t_dict = {}
+
 # Prepare cc18 data
 for data_id in openml.study.get_suite("OpenML-CC18").data:
     # Retrieve dataset
@@ -102,6 +104,14 @@ for data_id in openml.study.get_suite("OpenML-CC18").data:
     X, y, is_categorical, _ = dataset.get_data(
         dataset_format="array", target=dataset.default_target_attribute
     )
+
+    rf_acc_dict[data_id] = []
+    rf_train_t_dict[data_id] = []
+    rf_test_t_dict[data_id] = []
+
+    sdf_acc_dict[data_id] = []
+    sdf_train_t_dict[data_id] = []
+    sdf_test_t_dict[data_id] = []
 
     # Split the datasets into 5-fold CV
     skf = StratifiedKFold(shuffle=True)
@@ -117,14 +127,38 @@ for data_id in openml.study.get_suite("OpenML-CC18").data:
             rf_acc, rf_train_t, rf_test_t = experiment_rf(
                 X_train, X_test, y_train, y_test
             )
-            write_result("../results/rf/cc18_acc.txt", rf_acc)
-            write_result("../results/rf/cc18_train_t.txt", rf_train_t)
-            write_result("../results/rf/cc18_test_t.txt", rf_test_t)
+            rf_acc_dict[data_id].append(rf_acc)
+            rf_train_t_dict[data_id].append(rf_train_t)
+            rf_test_t_dict[data_id].append(rf_test_t)
+
+            f = open("../results/rf/cc18_acc.json", "w")
+            json.dump(rf_acc_dict, f)
+            f.close()
+
+            f = open("../results/rf/cc18_train_t.json", "w")
+            json.dump(rf_train_t_dict, f)
+            f.close()
+
+            f = open("../results/rf/cc18_test_t.json", "w")
+            json.dump(rf_test_t_dict, f)
+            f.close()
 
         if args.all or args.sdf:
             sdf_acc, sdf_train_t, sdf_test_t = experiment_sdf(
                 X_train, X_test, y_train, y_test
             )
-            write_result("../results/sdf/cc18_acc.txt", sdf_acc)
-            write_result("../results/sdf/cc18_train_t.txt", sdf_train_t)
-            write_result("../results/sdf/cc18_test_t.txt", sdf_test_t)
+            sdf_acc_dict[data_id].append(sdf_acc)
+            sdf_train_t_dict[data_id].append(sdf_train_t)
+            sdf_test_t_dict[data_id].append(sdf_test_t)
+
+            f = open("../results/sdf/cc18_acc.json", "w")
+            json.dump(sdf_acc_dict, f)
+            f.close()
+
+            f = open("../results/sdf/cc18_train_t.json", "w")
+            json.dump(sdf_train_t_dict, f)
+            f.close()
+
+            f = open("../results/sdf/cc18_test_t.json", "w")
+            json.dump(sdf_test_t_dict, f)
+            f.close()

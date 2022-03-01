2258ff6a0d0dd65193f4ac4e3623cfeb8377247b
Haoyin Xu <haoyinxu@gmail.com>
2022-03-01 09:00:51 -0500

    ENH update variable name for node counts

--- benchmarks/code/toolbox.py
+++ benchmarks/code/toolbox.py
@@ -16,3 +16,15 @@ def prediction(classifier, X_test, y_test):
     y_preds = classifier.predict(X_test)
 
     return accuracy_score(y_preds, y_test)
+
+
+def node_count(classifier, forest=False):
+    """Records the number of nodes"""
+    num_nodes = 0
+    if forest:
+        for tree in classifier.estimators_:
+            num_nodes += tree.tree_.node_count
+    else:
+        num_nodes = classifier.tree_.node_count
+
+    return num_nodes
